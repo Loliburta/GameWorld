51a84f73a26db97a8f6d87e30f7c518e4257fc59
Loliburta <mateusz.maranowski@protonmail.com>
2021-06-16 19:51:30 +0200

    added navbar for mobile and navbar resizing

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,22 @@
 import Logo from "../../svg/Logo.svg";
+import { Icon } from "@iconify/react";
+import searchIcon from "@iconify-icons/bx/bx-search";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
       <img src={Logo} alt="Logo" className="navbar__logo" />
-      <input
-        type="text"
-        placeholder="Type a game title..."
-        className="navbar__search"
-      ></input>
+      <div className="navbar__searchBox">
+        <input
+          id="search"
+          type="text"
+          placeholder="Type a game title..."
+          className="navbar__searchBox__input"
+        ></input>
+        <label htmlFor="search" className="navbar__searchBox__icon">
+          <Icon icon={searchIcon} className="navbar__searchBox__icon__svg" />
+        </label>
+      </div>
     </div>
   );
 };
